cf261901d8af2356b0fcc7a007542fbb4bd49d45
louyulin <1245337273@qq.com>
2018-01-18 21:02:40 +0800

    提交吐司

--- app/src/main/java/com/example/lyl/diytoast/MainActivity.java
+++ app/src/main/java/com/example/lyl/diytoast/MainActivity.java
@@ -1,14 +1,32 @@
 package com.example.lyl.diytoast;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.TextView;
+
+import com.example.lyl.mylibrary.LToast;
 
 public class MainActivity extends AppCompatActivity {
+
+    private int index = 0;
+    private TextView tv;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        initView();
+    }
+
+    private void initView() {
+        tv = (TextView) findViewById(R.id.tv);
+        tv.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                index ++ ;
+                LToast.showToast(MainActivity.this,index + "");
+            }
+        });
     }
 }
-git
---
+++ mylibrary/src/main/java/com/example/lyl/mylibrary/LToast.java
@@ -0,0 +1,21 @@
+package com.example.lyl.mylibrary;
+
+
+import android.content.Context;
+import android.widget.Toast;
+
+public class LToast {
+    private static Toast toast;
+
+    public static void showToast(Context context,
+                                 String content) {
+        if (toast == null) {
+            toast = Toast.makeText(context,
+                    content,
+                    Toast.LENGTH_SHORT);
+        } else {
+            toast.setText(content);
+        }
+        toast.show();
+    }
+}
